67e13129576596f991acc815485aff1f6ea61722
jw <lukeeau@hotmail.com>
2016-09-05 14:30:34 +0800

    with html templates

--- blog/urls.py
+++ blog/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
+from blogpost.api import BlogpostSet
 
+
+apiRouter = routers.DefaultRouter()
+apiRouter.register(r'blogpost', BlogpostSet)
+
+'''
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+'''
+
+urlpatterns = [
+    url(r'^$', 'blogpost.views.index'),
+    url(r'^blog/(?P<slug>[^\.]+).html', 'blogpost.views.view_post', name='view_blog_post'),
+    url(r'^admin/', include(admin.site.urls))
 ]
--- blogpost/views.py
+++ blogpost/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_post(request, slug):
+    return render_to_response('blogpost_detail.html', {
+        'post': get_object_or_404(Blogpost, slug=slug)
+    })
+
